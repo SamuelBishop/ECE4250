8009962bd482e49b3a049f333f7bf170fc84fc62
Samuel <sjbc35@mail.missouri.edu>
2020-04-18 18:54:30 -0500

    Added content to my last push. Oops lol.

--- LAB8/fullAdder.vhd
+++ LAB8/fullAdder.vhd
@@ -1,1 +1,13 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
 
+entity fullAdder is
+		port(X,Y,Cin: in std_logic; -- Inputs
+			Cout, Sum: Out std_logic);
+end fullAdder;
+
+Architecture behave of fullAdder is
+begin
+	Cout <= (X and Y) or (X and Cin) or (Y and Cin) after 10 ns;
+	Sum <= X xor Y xor Cin after 10 ns;
+end behave;
--- LAB8/twosComp.vhd
+++ LAB8/twosComp.vhd
@@ -1,1 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
 
+entity twosComp is
+	generic(n : integer := 8);
+	port (
+		A : in std_logic_vector(n+(n-1)*2 downto 0);
+		Z : out std_logic_vector(n+(n-1)*2 downto 0)
+	);
+end entity twosComp;
+
+architecture behave of twosComp is
+	signal atTwoCompl : std_logic_vector(n+(n-1)*2 downto 0);
+
+	begin
+	process(A, atTwoCompl)
+		begin
+			AtwoCompl <= not(A) + '1';
+			Z <= atTwoCompl;
+	end process;
+
+end architecture behave;
