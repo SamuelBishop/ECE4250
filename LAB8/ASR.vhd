3bbcfcf41b47ffc2d25bde1d7eec0438f101af01
Samuel <sjbc35@mail.missouri.edu>
2020-04-18 19:53:36 -0500

    Changed the ASR circuit for our needs.

--- LAB8/ASR.vhd
+++ LAB8/ASR.vhd
@@ -4,15 +4,15 @@ use ieee.numeric_std.all;
 
 entity ASR is
 	port(
-		signal r_Shift1     : in std_logic_vector(3 downto 0) := "1000";
-  		signal r_Unsigned_R : out unsigned(3 downto 0)        := "0000";
-  		signal r_Signed_R   : out signed(3 downto 0)          := "0000"
+		signal toShift     : in std_logic_vector(3 downto 0);
+  		signal unsignedOut : out unsigned(3 downto 0);
+  		signal signedOut   : out signed(3 downto 0)
 	);  
 end ASR;
 
 architecture behavior of ASR is
 begin
     -- Right Shift
-	r_Unsigned_R <= shift_right(unsigned(r_Shift1), 2);
-	r_Signed_R   <= shift_right(signed(r_Shift1), 	2);
+	unsignedOut <= shift_right(unsigned(toShift), 1);
+	signedOut   <= shift_right(signed(toShift),   1);
 end behavior;
