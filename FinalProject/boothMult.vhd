fd07ae700b5a382be3d469961ea9adcaae36d3c7
Samuel <sjbc35@mail.missouri.edu>
2020-04-28 19:49:24 -0500

    Code compiles for booth's multiplication algorithm. Components and output are not implemented correctly yet.

--- FinalProject/boothMult.vhd
+++ FinalProject/boothMult.vhd
@@ -7,10 +7,10 @@ entity boothMult is
 		-- Inputs
 		M, Q, A: in std_logic_vector(3 downto 0); -- 4 bit Multiplicand
 		Idx: in integer range 0 to 9;
-		N: in integer range 0 to 9;
+		N: inout integer range 0 to 9;
 		-- Outputs
 		Output: out std_logic_vector(7 downto 0); --8 bit
-		Carry: in std_logic; --1 bit		
+		Carry: in std_logic --1 bit		
 	);
 end boothMult;
 
@@ -21,23 +21,24 @@ architecture structure of boothMult is
 			A,B: in std_logic_vector(3 downto 0);
 			Ci: in std_logic; -- Inputs
 			S: out std_logic_vector(3 downto 0);
-			Co: out std_logic); -- Output
+			Co: out std_logic -- Output
 		);
 	end component;
 
 	component subtractor4
 		port(
-			A,B: in std_logic_vector(5 downto 0);
+			A,B: in std_logic_vector(3 downto 0);
 			Bi: in std_logic; -- Inputs
-			D: out std_logic_vector(5 downto 0);
-			Bo: out std_logic); -- Output
+			D: out std_logic_vector(3 downto 0);
+			Bo: out std_logic -- Output
 		);
 	end component;
 
 	component ASR
 		port(
-			signal toShift     : in std_logic_vector(3 downto 0);
-  			signal unsignedOut : out unsigned(3 downto 0)
+			toShift     : in std_logic_vector(3 downto 0);
+  			unsignedOut : out unsigned(3 downto 0);
+			outbit		: out std_logic
 		);
 	end component;
 
@@ -50,50 +51,58 @@ architecture structure of boothMult is
 
 
 	------------------- 01. SIGNAL/STATE DEFINITION -------------------
-	signal state: integer range 7 to 1:= 0;
-	signal adderCarry, adderCO, subtractorCarry, subtractorCO: bit;
-	signal adderResult, subtractorResult, shiftResult: std_logic_vector(3 downto 0);
-	
-	------------------- 02. LOGIC IMPLEMENTATION -------------------
+	signal state: integer range 0 to 6:= 0;
+	signal Q0, adderCarry, adderCO, shiftCarry, subtractorCarry, subtractorCO: std_logic := '0';
+	signal shiftResult: unsigned(3 downto 0);
+	signal AddSubResult: std_logic_vector(3 downto 0);
+	signal ending: std_logic := '0';
+
+	-- Implementation of components via portmapping
 	begin
+	ADDER: adder4 port map (A, M, adderCarry, AddSubResult, adderCO);
+	SUBTRACTOR: subtractor4 port map (A, M, subtractorCarry, AddSubResult, subtractorCO);
+	RSHIFT: ASR port map (AddSubResult, shiftResult, shiftCarry);
+
+	------------------- 02. LOGIC IMPLEMENTATION -------------------
 			process(state) 
-				begin
-					-- Start the state logic
+			begin
+				-- Start the state logic
 					case state is
 					
 						-- State 1: q1q0 check
-						when 1=> if ...q1q0 -> 10... then state <= 2;
-							elsif ...q1q0 -> 01... then state <= 3;
-							else state <= 4;
-							endif;
+						when 0 => if Q(0) = '1' and Q0 = '1' then state <= 1;
+							elsif Q(0) = '0' and Q0 = '1'then state <= 2;
+							else state <= 3;
+							end if;
 					
 						-- State 2: Subtraction
-						when 2=> ...Perform subtraction of M from A... then state <= 4;
+						when 1 => state <= 3;
 
 						-- State 3: Addition
-						when 3=> ...Perform addition of M and A... then state <= 4;
+						when 2 => state <= 3;
 
 						-- State 4: ASR
-						when 4=> ...Perform ASR... then state <= 5;
+						when 3 => state <= 4;
 
 						-- State 5: Decrement n
-						when 5=> ...Decrement n... then state <= 6;
+						when 4 => state <= 5;
 
 						-- State 6: Check 0
-						when 6=> if ...n=0... then state <= 7;
-							elsif ...n!=0... then state <= 1;
-							endif;
+						when 5 => if N = 0 then state <= 6;
+							else state <= 1; 
+							end if;
 
 						-- State 7: Report AQ
-						when 7=> ...Report AQ... then state <= STOP;
+						when 6 => ending <= '1';
 					end case;
-				end if;
 			end process;
 
-		-- Implementation of components via portmapping
-		ADDER: adder4 port map (A, M, adderCarry, adderResult, adderCO);
-		SUBTRACTOR: subtractor4 port map (A, M, subtractorCarry, subtractorResult, subtractorCO);
-		RSHIFTADD: ASR port map (adderResult, shiftResult);
-		RSHIFTSUB: ASR port map (subtractorResult, shiftResult);
+	-- Do all conditional statements here
+	-- 2) AddSubResult => (A - M);
+	AddSubResult <= A when (state = 2);
+	-- 3) AddSubResult => (A + M);
+	AddSubResult <= A when (state = 3);
+	-- 5) N => N-1;
+	N <= N - 1 when (state = 5);
 
 end structure;
